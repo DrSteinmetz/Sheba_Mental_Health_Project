a839a7994fe044c51aea6fd38e2d6b8536c062fd
Matan <matan199412@gmail.com>
2020-11-30 14:48:24 +0200

    login repo

--- app/src/main/java/com/example/sheba_mental_health_project/repository/AuthRepository.java
+++ app/src/main/java/com/example/sheba_mental_health_project/repository/AuthRepository.java
@@ -2,9 +2,13 @@ package com.example.sheba_mental_health_project.repository;
 
 import android.content.Context;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AuthRepository {
 
     private static AuthRepository authRepository;
+    private AuthRepoLoginPatientInterface mAuthRepoLoginPatientInterface;
+    private FirebaseAuth mAuth;
 
     final Context mContext;
 
@@ -12,11 +16,20 @@ public class AuthRepository {
     public static AuthRepository getInstance(final Context context) {
         if (authRepository == null) {
             authRepository = new AuthRepository(context);
+
         }
         return authRepository;
     }
 
     private AuthRepository(Context mContext) {
         this.mContext = mContext;
+        this.mAuth = FirebaseAuth.getInstance();
+
     }
+
+    public interface AuthRepoLoginPatientInterface{
+        void onPatientLoginSucceed();
+        void onPatientLoginFailed();
+    }
+   
 }
